e3add5d46ee2d885b19819a12edbbcd629151e86
MAple <1952972500@qq.com>
2024-04-08 14:31:40 +0800

    首页瀑布流

--- miniProgram/pages/home/index.js
+++ miniProgram/pages/home/index.js
@@ -1,29 +1,13 @@
 // index.js
 
 import { getLandscapeImages } from "./data";
-
-function getNewList() {
-  const newList = new Array(20).fill(0);
-  const imgUrlList = getLandscapeImages();
-  let count = 0;
-  for (let i = 0; i < newList.length; i++) {
-    newList[i] = {
-      id: i + 1,
-      title: `scroll-view`,
-      time: `19:20`,
-      like: 88,
-      image_url: imgUrlList[count++ % imgUrlList.length],
-    };
-  }
-  return newList;
-}
+import util from "../../utils/util.js";
 
 Page({
   data: {
     times: 0,
-    // 底部列表数据
-    bottomList: getNewList(),
-
+    token: "",
+    noteList: [],
     // 网络布局参数
     crossAxisCount: 2,
     crossAxisGap: 8,
@@ -36,20 +20,90 @@ Page({
   },
   onLoad() {
     const res = wx.getMenuButtonBoundingClientRect();
+    let gottoken = wx.getStorageSync("token");
     this.setData({
       menuTop: res.top,
       menuHeight: res.height,
       menuLeft: res.width + 10,
-      times: 0,
+      token: gottoken || "",
+      noteList: getNewList(),
     });
   },
-  bindSrollToLower() {
-    console.log(this.data)
-    if (this.data.times < 10) {
-      this.setData({
-        times:this.data.times+1,
-        bottomList: this.data.bottomList.concat(getNewList()),
+
+getNewList: async function () {
+  let that = this;
+  let time = util.formatTime(new Date(), "YYYY-mm-dd HH:mm:ss");
+  let noteListQuery = {
+    loginKey: {
+      auth: "user",
+      token: that.data.token,
+    },
+    beforeWhen: time,
+    listLength: 100,
+  };
+
+  try {
+    let res = await new Promise((resolve, reject) => {
+      wx.request({
+        url: "https://ctrip.x3322.net:3000/travelDiary/getNoteComments",
+        method: 'POST',
+        data: noteListQuery,
+        header: {
+          "content-type": "application/json", // 默认值
+          "Authorization": that.data.token,
+        },
+        success(res) {
+          resolve(res);
+        },
+        fail(err) {
+          reject(err);
+        }
       });
-    }
+    });
+
+    console.log(res.data);
+    let newList = res.data;
+    newList = newList.map((item) => {
+      item.pastTime = util.formatPast(
+        new Date(item.uploadTime.replaceAll("-", "/")),
+        "YYYY-mm-dd"
+      );
+      return item;
+    });
+    return newList;
+  } catch (err) {
+    console.log("error:", err);
+    return [];
+  }
+},
+    // const newList = new Array(20).fill(0);
+    // const imgUrlList = getLandscapeImages();
+    // let count = 0;
+    // for (let i = 0; i < newList.length; i++) {
+    //   newList[i] = {
+    //     noteId: i + 1,
+    //     title: `helllo ${i}`,
+    //     uploadTime: `2024-04-06 12:12:12`,
+    //     likeNume: 88,
+    //     coverImg: imgUrlList[count++ % imgUrlList.length],
+    //     authorAvatar:
+    //       "https://res.wx.qq.com/op_res/lS41C5Xp6y6mfUbelCW8PArEcMwWRuhSohPO46vAiELbhAf56_CwONEDgM2vIVxOlT5KDcSxCkV8xIJ6cg3x2Q",
+    //     authorNickname: "小明",
+    //   };
+    //   newList[i].pastTime = util.formatPast(
+    //     new Date(newList[i].uploadTime.replaceAll("-", "/")),
+    //     "YYYY-mm-dd"
+    //   );
+    // }
+    // return newList;
+  bindSrollToLower: function () {
+    this.setData({
+      noteList: this.data.noteList.concat(getNewList()),
+    });
   },
+  bindScrollToUpper:function(){
+    this.setData({
+      noteList: this.data.noteList.concat(getNewList()),
+    });
+  }
 });
--- miniProgram/utils/util.js
+++ miniProgram/utils/util.js
@@ -1,19 +1,55 @@
-const formatTime = date => {
-  const year = date.getFullYear()
-  const month = date.getMonth() + 1
-  const day = date.getDate()
-  const hour = date.getHours()
-  const minute = date.getMinutes()
-  const second = date.getSeconds()
+const formatTime = (date, format) => {
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
+  const hour = date.getHours();
+  const minute = date.getMinutes();
+  const second = date.getSeconds();
+  if (format === "YYYY-mm-dd HH:mm:ss")
+    return `${[year, month, day].map(formatNumber).join("-")} ${[
+      hour,
+      minute,
+      second,
+    ]
+      .map(formatNumber)
+      .join(":")}`;
+  else if (format === "YYYY-mm-dd")
+    return `${[year, month, day].map(formatNumber).join("-")}`;
+};
 
-  return `${[year, month, day].map(formatNumber).join('/')} ${[hour, minute, second].map(formatNumber).join(':')}`
-}
+const formatPast = (date, format) => {
+  // 传入格式处理、存储转换值
+  let t = date.getTime(),s;
+  // 获取js 时间戳
+  let time = new Date().getTime();
+  console.log("hello");
+  // 当前时间戳 - 传入时间戳
+  time = Number.parseInt(`${time - t}`);
+  if (time < 10000) {
+    return "刚刚";
+  } else if (time < 60000 && time >= 10000) {
+    s = Math.floor(time / 1000);
+    return `${s}秒前`;
+  } else if (time < 3600000 && time >= 60000) {
+    s = Math.floor(time / 60000);
+    return `${s}分钟前`;
+  } else if (time < 86400000 && time >= 3600000) {
+    s = Math.floor(time / 3600000);
+    return `${s}小时前`;
+  } else if (time < 259200000 && time >= 86400000) {
+    s = Math.floor(time / 86400000);
+    return `${s}天前`;
+  } else {
+    return formatTime(date, format);
+  }
+};
 
-const formatNumber = n => {
-  n = n.toString()
-  return n[1] ? n : `0${n}`
-}
+const formatNumber = (n) => {
+  n = n.toString();
+  return n[1] ? n : `0${n}`;
+};
 
 module.exports = {
-  formatTime
-}
+  formatTime,
+  formatPast,
+};
