f16439d88e1258115aed618665e18a01f1a738a6
Timelordregeneration10 <3497049745@qq.com>
2024-04-11 17:03:20 +0800

    发布游记page

--- miniProgram/pages/createStory/index.js
+++ miniProgram/pages/createStory/index.js
@@ -1,22 +1,25 @@
 // pages/createStory/index.js
+import Api from "../../utils/api";
 Page({
   /**
    * 页面的初始数据
    */
   data: {
     mediaList: [],
-    inputValue: "",
+    diaryTitle: "",
+    diaryContent: "",
     mode: "create",//create or edit
     // 顶部布局参数
     menuTop: 0,
     menuHeight: 0,
     menuLeft: 0,
+    uploadPicDisplay: "flex",
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad:async function(options) {
+  onLoad: async function (options) {
     const res = wx.getMenuButtonBoundingClientRect();
     this.setData({
       menuTop: res.top,
@@ -24,23 +27,25 @@ Page({
       menuLeft: res.width + 10,
       mode: "create"
     });
-    if(options.noteId){
+    if (options.noteId) {
       this.setData({
-        mode:"edit"
+        mode: "edit"
       })
       const res = await Api.getNoteDetails({
-        noteId:options.noteId
+        noteId: options.noteId
       });
     }
 
   },
-  bindTextAreaBlur: function (e) {
-    this.setData({
-      inputVal: e.detail.value,
-    });
+  handleTitleChange(e) {
+    this.data.diaryTitle = e.detail.value;
+  },
+  handleContentChange(e) {
+    this.data.diaryContent = e.detail.value;
   },
   bindChooseMedia: function () {
     if (this.data.mediaList.length >= 9) {
+      this.setData({ uploadPicDisplay: "none" })
       wx.showToast({
         title: "最多上传9张图片",
         icon: "none",
@@ -48,6 +53,7 @@ Page({
       });
       return;
     }
+    this.setData({ uploadPicDisplay: "flex" })
     let remainChooseCount = 9 - this.data.mediaList.length;
     wx.chooseMedia({
       count: remainChooseCount,
@@ -55,52 +61,148 @@ Page({
       mediaType: ["image", "video"],
       sourceType: ["album", "camera"],
       success: (res) => {
-        let {	tempFiles} = res;
-        tempFiles = tempFiles.forEach((item) => {
-          item.type = item.type === "video" ? "video" : "image";
-        });
+        let { tempFiles } = res;
+        // console.log(tempFiles);
+        let tempArray = this.data.mediaList;
+        for (let i = 0; i < tempFiles.length; i++) {
+          tempArray.push({
+            tempFilePath: tempFiles[i].tempFilePath,
+            fileType: tempFiles[i].fileType,
+            url: "",
+          })
+          wx.uploadFile({
+            filePath: tempFiles[i].tempFilePath,
+            name: 'name',
+            url: 'https://ctrip.x3322.net:3000/api/travelDiary/uploadFile',
+            formData: {
+              mediaType: tempFiles[i].fileType == "image" ? "img" : "video"
+            },
+            success(res) {
+              if (res.statusCode === 200) {
+                const { url, mediaType } = res.data;
+                tempArray[tempArray.length - 1].url = url;
+              }
+            }
+          })
+        }
+        // console.log(tempArray);
         this.setData({
-          photoList: res.tempFilePaths,
+          mediaList: tempArray,
         });
+        if (tempArray.length >= 9) {
+          this.setData({ uploadPicDisplay: "none" })
+        }
       },
     });
   },
-  bindSubmit:function(){},
-  bindPreviewMedia:function(){},
-  catchDelMedia:function(){},
+  //先不做
+  bindPreviewMedia: function () { },
+  catchDelMedia: function (e) {
+    if (this.data.mediaList.length >= 9) {
+      this.setData({
+        uploadPicDisplay: "flex"
+      })
+    }
+    let tempArray = this.data.mediaList;
+    tempArray.splice(e.target.dataset.index, 1);
+    this.setData({
+      mediaList: tempArray
+    })
+    // console.log(this.data.mediaList);
+  },
+  bindSubmit: async function () {
+    if (this.data.diaryTitle == "") {
+      wx.showToast({
+        title: "请输入故事标题！",
+        icon: "none",
+        duration: 2000,
+      });
+      return;
+    }
+    if (this.data.diaryContent == "") {
+      wx.showToast({
+        title: "请输入故事内容！",
+        icon: "none",
+        duration: 2000,
+      });
+      return;
+    }
+    if (this.data.mediaList.length == 0) {
+      wx.showToast({
+        title: "请上传图片或视频！",
+        icon: "none",
+        duration: 2000,
+      });
+      return;
+    }
+    let tempArray = [];
+    for (let i = 0; i < this.data.mediaList.length; i++) {
+      tempArray.push({
+        fileType: this.data.mediaList[i].fileType == "image" ? "img" : "video",
+        url: this.data.mediaList[i].url,
+      })
+    }
+    let res = await Api.uploadNote({
+      content: {
+        noteTitle: this.data.diaryTitle,
+        noteContent: this.data.noteContent,
+        location: "",
+        resources: tempArray,
+      }
+    })
+    if (res.data.status === 200) {
+      wx.showToast({
+        title: '发布成功',
+        icon: 'success',
+        duration: 2000
+      })
+      setTimeout(() => {
+        wx.navigateTo({
+          url: `/pages/storyDetail/index?noteId=${res.data.noteId}`,
+        })
+      }, 2000)
+    }
+    else {
+      wx.showToast({
+        title: '发布失败',
+        icon: 'error',
+        duration: 2000,
+      })
+    }
+  },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady() {},
+  onReady() { },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow() {},
+  onShow() { },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide() {},
+  onHide() { },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload() {},
+  onUnload() { },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh() {},
+  onPullDownRefresh() { },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom() {},
+  onReachBottom() { },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage() {},
+  onShareAppMessage() { },
 });
